6076abc56978ef751caed1f2980859b51fe7b2df
Ali jawwad <33836051+jawwad-ali@users.noreply.github.com>
2024-11-10 21:09:49 +0500

    Flexbox using Tailwind CSS

---
+++ my-app/app/components/HeroSection.tsx
@@ -0,0 +1,11 @@
+const HeroSection = () => {
+  return (
+    <main className="flex gap-8 mx-10">
+        <div className="w-1/2 bg-orange-950 h-96"></div>
+        <div className="w-6/12 bg-purple-950 h-96"></div>
+    </main>
+
+    )
+}
+
+export default HeroSection
